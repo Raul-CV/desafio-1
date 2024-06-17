0af07fd10e062026113624731995ea02488a3a29
Raul <cabreraraul1196@gmail.com>
2024-06-16 22:26:42 -0400

    se comenta main

--- main.tf
+++ main.tf
@@ -11,20 +11,20 @@ resource "aws_instance" "servidor-nginx" {
     Name = "NginxServer"
   }
 
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt update",
-      "sudo apt install -y nginx",
-      "echo 'Hello World' | sudo tee /var/www/html/index.html"
-    ]
+#   provisioner "remote-exec" {
+#     inline = [
+#       "sudo apt update",
+#       "sudo apt install -y nginx",
+#       "echo 'Hello World' | sudo tee /var/www/html/index.html"
+#     ]
 
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"  # Cambia esto según el AMI que estés usando
-      private_key = file("fpY8ovQbvH5TthEY21i7rjvkLEDatcv4aCtfZAg0JSk")  # Ruta a tu clave privada
-      host        = aws_instance.servidor-nginx.public_ip
-    }
-  }
+#     connection {
+#       type        = "ssh"
+#       user        = "ec2-user"  # Cambia esto según el AMI que estés usando
+#       private_key = file("fpY8ovQbvH5TthEY21i7rjvkLEDatcv4aCtfZAg0JSk")  # Ruta a tu clave privada
+#       host        = aws_instance.servidor-nginx.public_ip
+#     }
+#   }
 }
 
 output "public_ip" {
